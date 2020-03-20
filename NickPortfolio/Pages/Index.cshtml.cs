61993b0b9d1ebf5c4481619986e5d926dd325825
Nick Guerra <nickg@maliciousgames.com>
2020-03-20 06:07:46 -0500

    tweaked color

--- NickPortfolio/Pages/Index.cshtml.cs
+++ NickPortfolio/Pages/Index.cshtml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using NickPortfolio.Models;
+
 
 namespace NickPortfolio.Pages
 {
@@ -24,38 +24,7 @@ namespace NickPortfolio.Pages
       {
 
       }
-
-      [BindProperty]
-      public Email sendMail { get; set; }
-
-      // TODO: Implement Razor Page contact form
-      public async Task OnPost()
-      {
-
-         using (MailMessage mail = new MailMessage())
-         {
-            string To = "guerratechinfo@gmail.com";
-            string Subject = sendMail.Subject; // Need to add to form
-            string Body = sendMail.Message;
-            mail.From = new MailAddress(""); // user enterned variable passed in
-            mail.To.Add(To);
-            mail.Subject = Subject;
-            mail.Body = Body;
-            mail.IsBodyHtml = false;
-
-            using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
-            {
-               smtp.UseDefaultCredentials = false;
-               smtp.Credentials = new NetworkCredential("guerratechinfo@gmail.com", "&&&&&&&");
-               smtp.EnableSsl = true;
-               smtp.Send(mail);
-            }
-
-         }
-
-      }
-
-
-
+    
    }
+ 
 }
